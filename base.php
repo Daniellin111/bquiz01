5369648bf7999c0db7fec009b21ecbb9111da6ae
Daniellin <adio88158@gmail.com>
2021-08-24 14:21:55 +0800

    初始化類別

--- base.php
+++ base.php
@@ -1,91 +1,28 @@
-<?php
+<?php 
 
-class DB {
+class DB {                                            ##資料庫
+      private $dsn="mysql:host=localhost;charset=utf8;dbname=db01";
+      private $root='root';
+      private $password='12345';
+      private $table;
+      private $pdo;
 
-private $dsn="mysql:host=localhost;charset=utf8;dbname=db01";
-private $root="root";
-private $password="12345";
-private $table;
-private $pdo;
+      public function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,$this->root,$this->password);
+      }
+      
+      // ...$arg 不定參數，使所有的參數都放入到陣列中
+      public function all(...$arg) {
+        $sql="select * from $this->table ";
 
-public function __construct($table)
-{
-  $this->table=$table;
-  $this->pdo=new PDO($this->dsn,$this->root,$this->password);
-}
-
-
-public function all(...$arg){
-  $sql="select * from $this->table ";
-  // $arg=[]  or [陣列],[SQL字串],[陣列,SQL字串],
-
-  if(isset($arg[0])){
-      if(is_array($arg[0])){
-          //["欄位"=>"值","欄位"=>"值"]
-          //where `欄位`='值' && `欄位`='值'
-          //"欄位"=>"值" ====> `欄位`='值'
-
-          foreach($arg[0] as $key => $value){
-              $tmp[]=sprintf("`%s`='%s'",$key,$value);
-          }
-              $sql=$sql . " where " . implode(" && ",$tmp);
-      }else{
-          //當它是字串
-          $sql=$sql . $arg[0];
+        return $this->pdo->query($sql)->fetchAll();
       }
 
-      if(isset($arg[1])){
-          //當它是字串
-          $sql=$sql . $arg[1];
-      }
-
-  }
-
-  echo $sql;
-  return $this->pdo->query($sql)->fetchAll();
-
-}
-
-public function count(...$arg){
-  $sql="select count(*) from $this->table ";
-  
-  if(isset($arg[0])){
-      if(is_array($arg[0])){
-          
-        foreach($arg[0] as $key => $value){
-              $tmp[]=sprintf("`%s`='%s'",$key,$value);
-          }
-              $sql=$sql . " where " . implode(" && ",$tmp);
-      }else{
-
-        $sql=$sql . $arg[0];
-      }
-
-      if(isset($arg[1])){
-          $sql=$sql . $arg[1];
-      }
-
-  }
-
-  // echo $sql;
-  return $this->pdo->query($sql)->fetchColumn();
-
-}
-
-}
+};
+      ##串資料表
+$db = new 
 
 
 
-
-$admin = new DB ("admin");
-echo "<pre>";
-print_r($admin->all(['acc'=>'root' , 'pw'=>'1234' ]));
-echo "</pre>";
-
-// echo "<pre>";
-// print_r($admin->all(" where acc='root' "));
-// echo "</pre>";
-
-// echo "<pre>";
-// print_r($admin->all(" where `pw`='1234'","order by `id` DESC"));
-// echo "</pre>";
+?>
